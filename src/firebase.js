a1c54189ac348da8c82d924574318a29e7458c42
longnv <123456a@A>
2022-03-21 15:11:09 +0700

    add firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLNw8IzSJy8y1jW0Ig01JKq3zfav7ZcI0",
+  authDomain: "ecommerce-61839.firebaseapp.com",
+  projectId: "ecommerce-61839",
+  storageBucket: "ecommerce-61839.appspot.com",
+  messagingSenderId: "295983348262",
+  appId: "1:295983348262:web:3e648bc5634428a90004e7"
+};
+
+// Initialize Firebase
+//export
+const app = initializeApp(firebaseConfig);
+export const auth = firebase.auth()
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
