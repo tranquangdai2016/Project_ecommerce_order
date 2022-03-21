599d7478ecc520b4d07f492df6aed84d94bcbd3b
tranquangdai2016 <72093847+tranquangdai2016@users.noreply.github.com>
2022-03-21 09:58:28 +0700

    create componet Header,add navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes,Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Header from './components/nav/Header';
 
 const App = () => {
   return (
-    <Routes>
-      <Route  path="/" element={<Home />} />
-      <Route  path="/Login" element={<Login />} />
-      <Route  path="/Register" element={<Register />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route  path="/" element={<Home />} />
+        <Route  path="/Login" element={<Login />} />
+        <Route  path="/Register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
