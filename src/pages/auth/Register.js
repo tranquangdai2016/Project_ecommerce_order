2fa5ea49f3ffb89f2db6d2531fc75e22ee50cb69
longnv <123456a@A>
2022-03-21 15:29:03 +0700

    fix conconflict

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,10 +1,10 @@
-<<<<<<< HEAD
 import React, {useState} from 'react'
 
 const Register = () => {
     const [email, setEmail] = useState("");
 
     const handleSubmit = () => {
+        
         //
     }
     const registerForm = () => (
@@ -32,33 +32,6 @@ const Register = () => {
                     {registerForm()}
                 </div>
             </div>            
-=======
-import React, { useState } from 'react'
-
-const Register = () => {
-    const [email, setEmail] = useState("");
-    const handleSubmit = () => {
-
-    }
-    const registerForm = () => <form onSubmit={handleSubmit}>
-        <input type="email"  
-        value={email} 
-        onChange={e => setEmail(e.target.value)} 
-        className='form-control'
-        />
-        <button type="submit" className='btn btn-raised'>
-            Reigister / {email}
-        </button>
-    </form>
-    return (
-        <div className='container p-5'>
-            <div className='row'>
-                <div className='col-md-6 offset-md-3'>
-                    <h4>Register</h4>
-                    {registerForm()}
-                </div>
-            </div>
->>>>>>> 3f4ea4517aa7640a629d708a14e8212cab071d95
         </div>
     );
 };
