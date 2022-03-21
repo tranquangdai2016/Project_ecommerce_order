604ecb1f620a77c86decfb9a3924552fddee82d3
tranquangdai <tranquangdai2016@gmail.com>
2022-03-21 16:37:55 +0700

    Auto stash before merge of "main" and "origin/main"

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,28 +1,31 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import {auth} from '../../firebase';
+import {toast, ToastContainer} from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css"
 
 const Register = () => {
     const [email, setEmail] = useState("");
+    const handleSubmit = async (e) => {
+        e.prevenDefault();
+        const config = {
+            url: 'http://locaclhost:3000/register/complate',
+            handleCodeInApp: true
+        }
+        await auth.sendSignInLinkToEmail(email,config);
+        toast.success('Email is send to ${email}. Click links to registration')
+    };
 
-    const handleSubmit = () => {
-        
-        //
-    }
-    const registerForm = () => (
-        <form onSubmit={handleSubmit}>
-            <input 
-            type="email" 
-            className="form-control" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            autoFocus
-            />
-            <button 
-            type="submit" 
-            className="btn btn-raised">
-                Register
-            </button>
-        </form>
-    )
+    window.localStorage.setItem('emailForRegistration', email)
+    const registerForm = () => <form onSubmit={handleSubmit}>
+        <input type="email"  
+        value={email} 
+        onChange={e => setEmail(e.target.value)} 
+        className='form-control'
+        />
+        <button type="submit" className='btn btn-raised'>
+            Reigister / {email}
+        </button>
+    </form>
     return (
         <div className="container p-5">
             <div className="row">
