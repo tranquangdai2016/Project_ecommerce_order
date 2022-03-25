2bcb8eeb37b2307e81239ffa432d33c11d0bc47b
longnv <123456a@A>
2022-03-25 16:31:56 +0700

    *code auth/controllers-middlewares

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,24 @@
-exports.createOrUpdateUser = (req, res) => {
-    res.json({
-        data: "hey you hit create-or-update-user API endpoint",
-    });
+const User = require('../models/user')
+
+exports.createOrUpdateUser = async (req, res) => {
+   const {name, picture, email} = req.user;
+
+   const user = await User.findOneAndUpdate(
+       {email},
+        {name: email.split('@')[0], picture},
+         {new: true}
+         );
+
+   if(user) {
+       console.log('USER UPDATE', user);
+       res.json(user)
+   }else{
+       const newUser = await new User({
+           email,
+           name: email.split('@')[0],
+           picture,
+       }).save();
+       console.log('USER UPDATE', newUser);
+       res.json(newUser);
+   }
 };
