9054b36768a17ca95c42c9e2652edadcd4aa796f
longnv <123456a@A>
2022-03-29 09:41:22 +0700

    *them category models

---
+++ server/models/category.js
@@ -0,0 +1,20 @@
+const module = require('mongoose');
+
+const categorySchema = new mongoose.Schema({
+    nam: {
+        type: String,
+        trim: true,
+        required: 'Name is required',
+        minLength: [3, 'Too short'],
+        maxLength: [32, 'Too long']
+    },
+    slug: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        index: true,
+    },
+
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', categorySchema );
