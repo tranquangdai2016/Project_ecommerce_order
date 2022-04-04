c6a924f8bdd76c396a679a02234213163eda0a6a
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-04 23:53:48 +0700

    get value input search and redirect to page shop

--- server/components/forms/Search.js
+++ server/components/forms/Search.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { SeachOutlined } from '@ant-design/icons'
+import { SearchOutlined } from '@ant-design/icons'
 
 const Search = () => {
     const dispatch = useDispatch();
@@ -10,20 +10,30 @@ const Search = () => {
 
     const history = useHistory();
 
-    const handleChange = (0) = {
-        //
-    }
+    const handleChange = (e) => {
+        dispatch({
+            type: "SEARCH_QUERY",
+            payload: { text: e.target.value },
+        });
+    };
 
-    const handleSubmit = (0) = {
-        //
-    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        history.push(`/shop?${text}`);
+    };
 
-    return (
-        <form className='form-inline my-2 my-lg-0' onSubmit={handleSubmit}>
-            <input type="search" value={text} className='form-control mr-sm-2' placeholder='Seach' />
-            <SearchOulined onClick={handleSubmit} style={{ cursor: "pointer" }} />
-        </form>
-    )
+return (
+    <form className='form-inline my-2 my-lg-0' onSubmit={handleSubmit}>
+        <input
+            onChange={handleChange}
+            type="search"
+            value={text}
+            className='form-control mr-sm-2'
+            placeholder='Seach'
+        />
+        <SearchOutlined onClick={handleSubmit} style={{ cursor: "pointer" }} />
+    </form>
+)
 };
 
 export default Search;
