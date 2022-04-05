7aeb45acee33bfe7b8b1bb2774a05af2f21d98d1
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-05 23:55:34 +0700

    create filter price range and show product follow price filter

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -2,17 +2,30 @@ import React, { useEffect, useState } from 'react'
 import { getProductsByCount, fetchProductsByFilter } from '../functions/product'
 import { useSelector, useDispatch } from 'react-redux'
 import ProductCard from '../components/cards/ProductCard'
+import { Menu, Slider } from 'antd'
+import { DollarOutlined } from '@ant-design/icons'
+
+const { SubMenu, ItemGroup } = Menu;
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [price, setPrice] = useState([0, 0]);
+    const [ok, setOk] = useState(false);
 
+    let dispatch = useDispatch();
     let { search } = useSelector((state) => ({ ...state }));
     const { text } = search;
 
     useEffect(() => {
         loadAllProducts();
     }, []);
+
+    const fetchProducts = (arg) => {
+        fetchProductsByFilter(arg).then((res) => {
+            setProducts(res.data);
+        });
+    };
 
     //load prodcuts by default on page load
     const loadAllProducts = () => {
@@ -30,19 +43,45 @@ const Shop = () => {
         return () => clearTimeout(delayed);
     }, [text]);
 
-    const fetchProducts = (arg) => {
-        fetchProductsByFilter(arg).then((res) => {
-            setProducts(res.data);
-        })
-    }
+    //load products base on prive range
+    useEffect(() => {
+        fetchProducts({ price });
+    }, [ok]);
+
+    const handleSlider = (value) => {
+        dispatch({
+            type: "SEARCH_QUERY",
+            payload: { text: "" },
+        });
+        setPrice(value);
+        setTimeout(() => {
+            setOk(!ok);
+        }, 300);
+    };
 
     return (
         <div className='container-fluid'>
             <div className='row'>
-                <div className="col-md-3">
-                    search/filter menu
+                <div className="col-md-3 pt-2">
+                    <h4>Search/filter</h4>
+                    <hr />
+                    <Menu defaultOpenKeys={["1", "2"]} mode='inline'>
+                        <SubMenu key={"1"} title={
+                            <span className='h6'> <DollarOutlined />Price </span>
+                        }>
+                            <div>
+                                <Slider
+                                    className='ml-4 mr-4'
+                                    tipFormatter={(v) => `VNĐ${v}`}
+                                    range value={price}
+                                    onChange={handleSlider}
+                                    max='1000000000'
+                                />
+                            </div>
+                        </SubMenu>
+                    </Menu>
                 </div>
-                <div className="col-md-9">
+                <div className="col-md-9 pt-2">
                     {loading ? (
                         <h4 className="text-danger">Loading...</h4>
                     ) : (
