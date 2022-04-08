2781d08db0c53cf7fcf53ec15018b8a2d9b4b692
duongdo238 <duongdo238@gmail.com>
2022-04-08 11:16:57 +0700

    category page with product

--- client/src/pages/category/CategoryHome.js
+++ client/src/pages/category/CategoryHome.js
@@ -1,26 +1,49 @@
-import React, { useState, useEffect} from "react";
-import {getCategory} from '../../functions/category';
-import {Link} from 'react-router-dom';
-import ProductCard from '../../components/cards/ProductCard';
+import React, { useState, useEffect } from "react";
+import { getCategory } from "../../functions/category";
+import { Link } from "react-router-dom";
+import ProductCard from "../../components/cards/ProductCard";
 import CategoryList from "../../components/category/CategoryList";
 
-const CategoryHome = ({match}) =>{
-    const [category, setCategory] = useState({});
-    const [products, setProducts] = useState([]);
-    const [loading, setLoading] = useState(false);
 
-    const {slug} = match.params;
+const CategoryHome = ({ match }) => {
+  const [category, setCategory] = useState({});
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        setLoading(true);
-        getCategory(slug)
-        .then(c =>{
-            console.log(JSON.stringify(c.data, null, 4))
-            setCategory(c.data)
-        })
-    }, [])
+  const { slug } = match.params;
 
-    return <p>{match.params.slug}</p>
-}
+  useEffect(() => {
+    setLoading(true);
+    getCategory(slug).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setCategory(res.data.category);
+      setProducts(res.data.products);
+      setLoading(true);
+    });
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <div class="row">
+        <div class="col">
+          {loading ? (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              Loading...
+            </h4>
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              {products.legth}Product in "{category.name}" category
+            </h4>
+          )}
+        </div>
+      </div>
+      <div class="row">
+          {products.map((p)=> <div className="col" key={p._id}>
+              <ProductCard product={p} />
+          </div>)}
+      </div>
+    </div>
+  );
+};
 
 export default CategoryHome;
