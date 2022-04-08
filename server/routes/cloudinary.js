c6f95fdb610e997b51f8440eabc76ccaf73600d6
longnv <123456a@A>
2022-04-08 11:14:00 +0700

    [merge] fix conflict

--- server/routes/cloudinary.js
+++ server/routes/cloudinary.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require('express')
 
 const router = express.Router();
 //middlewares
 const {authCheck, adminCheck} = require ('../middlewares/auth');
-const {upload, remove} = require ('../controllers/cloudinary');
-router.post("/uploadimages", authCheck , adminCheck , upload);
-router.post("/removeimages", adminCheck, adminCheck , remove);
-module.exports= router;
+
+//controllers
+const { upload, remove } = require('../controllers/cloudinary')
+
+router.post('/uploadimages', authCheck, adminCheck, upload);
+router.post('/removeimage', authCheck, adminCheck , remove);
+
+
+module.exports = router;
