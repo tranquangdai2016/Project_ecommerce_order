861e2c870ad9a513b5353de11b03aa9a45f58e32
duongdo238 <duongdo238@gmail.com>
2022-04-09 17:55:49 +0700

    fix loi

--- client/src/components/category/CategoryList.js
+++ client/src/components/category/CategoryList.js
@@ -16,7 +16,7 @@ const CategoryList = () =>{
 
     const showCategories = () => categories.map((c)=>
     <div key={c._id} className="col btn btn-outlined-primary btn-lg btn-block btn-raised m-3">
-        <Link to={`/category/${slug}`}>{c.name}</Link>
+        <Link to={`/category/${c.slug}`}>{c.name}</Link>
     </div>)
 
     return(
