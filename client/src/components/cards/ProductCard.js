8df76eac1f4d15e0c4a814577c7cb746bc66bd2c
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-11 22:02:00 +0700

    add product to localStorage

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -3,10 +3,33 @@ import { Card } from "antd";
 import { EyeOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 // import laptop from "../../images/laptop.png";
 import { Link } from "react-router-dom";
+import _ from "lodash";
 
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+
+  const handleAddToCart = () => {
+    //create cart array
+    let cart = [];
+    if (typeof window != 'undefined') {
+      //Nếu có cart ở localstorage thì GET
+      if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'))
+      }
+      //push new product to cart
+      cart.push({
+        ...product,
+        count: 1
+      })
+      //remove duplicates
+      let unique = _.uniqWith(cart, _.isEqual)
+      //save to localstorage
+      //console.log('unique', unique);
+      localStorage.setItem("cart", JSON.stringify(unique));
+    }
+  }
+
   //destructure
   const { images, title, description, slug, price } = product;
   return (
@@ -23,9 +46,9 @@ const ProductCard = ({ product }) => {
           <EyeOutlined className="text-warning" />
           <br /> View Product
         </Link>,
-        <>
+        <a onClick={handleAddToCart}>
           <ShoppingCartOutlined className="text-danger" /> <br /> Add to card{" "}
-        </>,
+        </a>,
       ]}
     >
       <Meta
