f3df6bb8f912959ad52abc5d69f02a7c8dd95aa3
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-12 22:39:51 +0700

    show detail product in cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import laptop from "../../images/laptop.png";
+import ModalImage from "react-modal-image";
 
-const ProductCardInCheckout = ({p}) => {
+const ProductCardInCheckout = ({ p }) => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{width: "100px", height: "auto"}}>
+                        {
+                            p.image.length
+                                ?
+                                (<ModalImage small={p.images[0].url} large={p.images[0].url} />)
+                                :
+                                (<ModalImage small={laptop} large={laptop} />)
+                        }
+                    </div>
+                </td>
                 <td>{p.title}</td>
                 <td>{p.price}</td>
                 <td>{p.brand}</td>
