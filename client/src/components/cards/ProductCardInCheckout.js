e33ef6db1b5c210c483bf2faa29543dfcda7d708
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-12 23:43:36 +0700

    create delete product function to cart

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -2,6 +2,8 @@ import React from 'react'
 import laptop from "../../images/laptop.png";
 import { useDispatch } from 'react-redux'
 import ModalImage from "react-modal-image";
+import { toast } from 'react-toastify';
+import { CheckCircleOutlined, CloseCircleOutlined, CloseOutlined } from '@ant-design/icons';
 
 const ProductCardInCheckout = ({ p }) => {
     const colors = ["Black", "Brown", "Silver", "White", "Blue"];
@@ -59,6 +61,29 @@ const ProductCardInCheckout = ({ p }) => {
         }
     }
 
+    const handleRemove = () => {
+        let cart = [];
+        if (typeof window !== "undefined") {
+            if (localStorage.getItem('cart')) {
+                cart = JSON.parse(localStorage.getItem('cart'))
+            }
+
+            cart.map((product, i) => {
+                if (product._id === p._id) {
+                    cart.splice(i, 1);
+                }
+            })
+            //console.log('cart update count', cart)
+
+            localStorage.setItem('cart', JSON.stringify(cart))
+            dispatch({
+                type: "ADD_TO_CART",
+                payload: cart,
+            })
+        }
+    }
+
+
     return (
         <tbody>
             <tr>
@@ -76,9 +101,6 @@ const ProductCardInCheckout = ({ p }) => {
                 <td>{p.title}</td>
                 <td>{p.price}</td>
                 <td>{p.brand}</td>
-                <td className='text-center'>
-                    <input type="number" className='form-control' value={p.count} onChange={handleQuantityChange} name="" id="" />
-                </td> 
                 <td>
                     <select onChange={handleColorChange} name="color" className='form-control'>
                         {p.color ? <option value={p.color}>{p.color}</option> : <option value="">Select</option>}
@@ -87,8 +109,15 @@ const ProductCardInCheckout = ({ p }) => {
                             .map((c) => <option key={c} value={c}>{c}</option>)}
                     </select>
                 </td>
-                <td>Shipping icon</td>
-                <td>Delete icon</td>
+                <td className='text-center'>
+                    <input type="number" className='form-control' value={p.count} onChange={handleQuantityChange} />
+                </td>
+                <td className='text-center'>
+                    {p.shipping === "Yes" ? <CheckCircleOutlined className='text-success' /> : <CloseCircleOutlined className='text-danger' />}
+                </td>
+                <td className='text-center'>
+                    <CloseOutlined onClick={handleRemove} className='text-danger pointer' />
+                </td>
             </tr>
         </tbody>
     )
