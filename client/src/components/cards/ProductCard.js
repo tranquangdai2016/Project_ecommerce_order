a9d8691a885968cb74a82301cbbbb43f6b3134c0
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-13 23:34:18 +0700

    get product from localstorage and push new pro to cart

--- client/src/components/cards/ProductCard.js
+++ client/src/components/cards/ProductCard.js
@@ -8,38 +8,55 @@ import { Link } from "react-router-dom";
 const { Meta } = Card;
 
 const ProductCard = ({ product }) => {
+
+  const handleAddToCart = () => {
+    //create cart array
+    let cart = []
+    if (typeof window !== 'undefined') {
+      //if cart is in localstorage GET it
+      if (localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'))
+      }
+      //push new product to cart
+      cart.push({
+        ...product,
+        count: 1,
+      })
+    }
+  }
+
   //destructure
   const { images, title, description, slug, price } = product;
   return (
     <>
-    {product && product.ratings && product.ratings.length > 0 ? (
-          showAverage(product)
-        ) : (
-          <div className="text-center pt-1 pb-3">No rating yet</div>
-        )}
-    <Card
-      cover={
-        <img
-          src={images && images.length ? images[0].url : laptop}
-          style={{ height: "150px", objectFit: "cover" }}
-          className="p-1"
+      {product && product.ratings && product.ratings.length > 0 ? (
+        showAverage(product)
+      ) : (
+        <div className="text-center pt-1 pb-3">No rating yet</div>
+      )}
+      <Card
+        cover={
+          <img
+            src={images && images.length ? images[0].url : laptop}
+            style={{ height: "150px", objectFit: "cover" }}
+            className="p-1"
+          />
+        }
+        actions={[
+          <Link to={`/product/${slug}`}>
+            <EyeOutlined className="text-warning" />
+            <br /> View Product
+          </Link>,
+          <a onClick={handleAddToCart}>
+            <ShoppingCartOutlined className="text-danger" /> <br /> Add to card{" "}
+          </a>,
+        ]}
+      >
+        <Meta
+          title={`${title} - $${price}`}
+          description={`${description && description.substring(0, 40)}...`}
         />
-      }
-      actions={[
-        <Link to={`/product/${slug}`}>
-          <EyeOutlined className="text-warning" />
-          <br /> View Product
-        </Link>,
-        <>
-          <ShoppingCartOutlined className="text-danger" /> <br /> Add to card{" "}
-        </>,
-      ]}
-    >
-      <Meta
-        title={`${title} - $${price}`}
-        description={`${description && description.substring(0, 40)}...`}
-      />
-    </Card>
+      </Card>
     </>
   );
 };
