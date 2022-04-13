e31991e91572f5ff1af848d2ddd50ecbf8f234c3
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-14 00:49:02 +0700

    show image pro by model image

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react";
+import React from "react";
+import ModalImage from 'react-modal-image'
+import laptop from "../../images/laptop.png"
 
 const ProductCardInCheckout = () => {
     return (
         <tbody>
             <tr>
-                <td>Image</td>
+                <td>
+                    <div style={{width: "100px", height: "auto"}}>
+                        {p.images.length ? (
+                            <ModalImage small={p.images[0].url} large={p.images[0].url} />
+                        ) : (
+                            <ModalImage small={laptop} large={laptop} />
+                        )}
+                    </div>
+                </td>
                 <td>{p.title}</td>
-                <td>{p.price}</td>
+                <td>${p.price}</td>
                 <td>{p.brand}</td>
                 <td>{p.color}</td>
                 <td>{p.count}</td>
