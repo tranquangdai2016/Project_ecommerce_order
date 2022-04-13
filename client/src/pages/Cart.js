d8fe433be33138935180d8d9fbd497c1ea6ce055
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-14 00:24:04 +0700

    show total price in cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -7,11 +7,47 @@ const Cart = () => {
     const { user, cart } = useSelector((state) => ({ ...state }));
     const dispatch = useDispatch();
 
-    
+    const getTotal = () => {
+        return cart.reduce((currentValue, nextValue) => {
+            return currentValue + nextValue.count * nextValue.price
+        }, 0)
+    }
+
     return (
-        <div className="container-fluid">
+        <div className="container-fluid pt-2">
             <div className="row">
-                <h4>Cart</h4>
+                <div className="col-md-8">
+                    <h4>Cart / {cart.length} Product</h4>
+                    {!cart.length ? (<h4>
+                        No products in cart. <Link to="/shop">Continue Shopping</Link>
+                    </h4>) : (
+                        "Show cart items"
+                    )}
+                </div>
+                <div className="col-md-4">
+                    <h4>Order Summary</h4>
+                    <hr />
+                    <p>Products</p>
+                    {cart.map((c, i) => (
+                        <div key={i}>
+                            <p>{cart.title} x {cart.count} = ${cart.price * cart.count}</p>
+                        </div>
+                    ))}
+                    <hr />
+                    Total: <b>${getTotal()}</b>
+                    <hr />
+                    {
+                        user ? (
+                            <button className="btn btn-sm btn-primary mt-2">
+                                Proceed to checkout
+                            </button>
+                        ) : (
+                            <button className="btn btn-sm btn-primary mt-2">
+                                Login to checkout
+                            </button>
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
