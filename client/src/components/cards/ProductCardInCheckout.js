3d4926e598dd67f640f1dd7b0d0fafffbf037ca3
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-14 00:56:07 +0700

    show color select

--- client/src/components/cards/ProductCardInCheckout.js
+++ client/src/components/cards/ProductCardInCheckout.js
@@ -3,11 +3,18 @@ import ModalImage from 'react-modal-image'
 import laptop from "../../images/laptop.png"
 
 const ProductCardInCheckout = () => {
+    const colors = ["Black", "Brown", "Silver", "White", "Blue"];
+
+    const handleColorChange = () => {
+
+    }
+
+
     return (
         <tbody>
             <tr>
                 <td>
-                    <div style={{width: "100px", height: "auto"}}>
+                    <div style={{ width: "100px", height: "auto" }}>
                         {p.images.length ? (
                             <ModalImage small={p.images[0].url} large={p.images[0].url} />
                         ) : (
@@ -18,7 +25,20 @@ const ProductCardInCheckout = () => {
                 <td>{p.title}</td>
                 <td>${p.price}</td>
                 <td>{p.brand}</td>
-                <td>{p.color}</td>
+                <td>
+                    <select
+                        onChange={handleColorChange}
+                        className="form-control"
+                        name="color" id=""
+                    >
+                        {p.color ? <option value={p.color}>{p.color}</option> : <option>Select</option>}
+                        {colors
+                            .filter((c) => c !== p.color)
+                            .map((c) => (
+                                <option value={c} key={c}>{c}</option>
+                            ))}
+                    </select>
+                </td>
                 <td>{p.count}</td>
                 <td>Shipping</td>
                 <td>Delete Icon</td>
