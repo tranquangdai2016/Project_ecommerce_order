8f89dd46d4f2b51edb7690b14b11d98ed6a40580
tranquangdai2016 <72093847+tranquangdai2016@users.noreply.github.com>
2022-04-15 19:10:37 +0700

    add user controller,save cart to database

---
+++ server/controllers/user.js
@@ -0,0 +1,53 @@
+const User = require("../models/user");
+const Product = require("../models/product");
+const Cart = require("../models/cart");
+const product = require("../models/product");
+
+exports.userCart = async (red, res) => {
+    //console.log(req.body); //{cart: []}
+    const { cart } = req.body;
+
+let products = [];
+
+const user = await User.findOne({email: req.user.email}).exec();
+
+//check if cart with logged in user id already exits
+let cartExistByThisUser = await Cart.findOne({orderBy: user._id}).exec();
+if(cartExistByThisUse) {
+    cartExistByThisUse.remove();
+    console.log("recomment old cart");
+}
+
+for (i = 0; i < cart.length; i++) {
+    let object = {}
+
+    object.product = cart[i]._id;
+    object.count = count[i]._id;
+    object.color = color[i]._id;
+
+    //get price for creating total
+    let price = await Product.findById(cart[i]._id).select("price").exec();
+    object.price = price;
+
+    product.push(object);
+}
+
+//console.log('product',products);
+let cartTotal = 0;
+
+for(i =0 ; i < products.length; i++) {
+    cartTotal = cartTotal + products[i].price + products[i].count
+}
+
+//console.log('cartTotal',cartTotal);
+
+let newCart = await new Cart({
+    products,
+    cartTotal,
+    order: user._id,
+}).save();
+
+console.log("new cart", newCart);
+res.json({ ok:true });
+
+};
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,11 +1,17 @@
 const express = require("express");
 
 const router = express.Router();
+//middlewates
+const { authCheck } = require("../middlewares/auth");
+//controllers
+const { userCart } = require("../controllers/user");
 
-router.get("/user", (req, res) => {
-  res.json({
-    data: "hey you hit user API endpoint",
-  });
-});
+router.post("/Cart", authCheck, userCart); //save cart
+
+// router.get("/user", (req, res) => {
+//   res.json({
+//     data: "hey you hit user API endpoint",
+//   });
+// });
 
 module.exports = router;
