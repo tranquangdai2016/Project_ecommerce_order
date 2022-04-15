6fd3a6984ae79120179c13e2357448fca14b38d4
longnv <123456a@A>
2022-04-15 16:09:41 +0700

    [upadte] payment

--- client/src/pages/Payment.js
+++ client/src/pages/Payment.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import StripeCheckout from '../components/StripeCheckout';
+import '../stripe.css';
+
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_KEY);
 
 
 const Payment = () => {
     return(
         <div className="container p-5 text-center">
-            <p>Complete your purchase</p>
+            <h4> Complete your purchase</h4>
+            <Elements stripe={promise}>
+                <div className="col-md-8 offset-md-2">
+                    <StripeCheckout />
+                </div>
+            </Elements>
         </div>
     );
 };
