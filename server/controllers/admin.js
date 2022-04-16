aef0212137ab4cb345d5a1b119805c746f132334
duongdo238 <duongdo238@gmail.com>
2022-04-16 08:36:38 +0700

    orders status update for admin

---
+++ server/controllers/admin.js
@@ -0,0 +1,21 @@
+const User = require('../models/user');
+
+exports.orders = async (req, res) => {
+    let orders = await orders
+    .find({})
+    .sort('-createdAt')
+    .populate('products.product')
+    .exec()
+
+    res.json(orders);
+};
+
+exports.orderStatus= async (req, res) => {
+    const {orderId, orderStatus} = req.body;
+
+    let updated = await order
+    .findByIdAndUpdate(orderId, {orderStatus}, {new: true})
+    .exec()
+    
+    res.json(updated)
+}
