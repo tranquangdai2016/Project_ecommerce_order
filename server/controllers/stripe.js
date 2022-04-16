d13b79c55d91932170e3768c5f430144d286c1fd
long <longnv@pmr.vn>
2022-04-16 23:50:06 +0700

    [update]payment

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -2,25 +2,45 @@ const User = require('../models/user');
 const Cart = require('../models/cart');
 const Product = require('../models/product');
 // const Coupon = require('../models/coupon');
-const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 exports.createPaymentIntent = async (req, res) => {
+    // console.log(req.body)
+    // return;
+
+    const { couponApplied } = req.body
+
     //later apply coupon
     //later calculate price
 
     //1 find user
      const user = await User.findOne({email: req.user.email}).exec()
     //2 get user cart total
-    const { cartTotal } = await  Cart.findOne({ orderdBy: user._id }).exec();
+    const { cartTotal, totalAfterDiscount } = await  Cart.findOne({
+        orderdBy: user._id
+    }).exec();
 
-     console.log("CART TOTAL CHARGER", cartTotal);
+     // console.log("CART TOTAL", cartTotal, "AFTER DIS%", totalAfterDiscount);
+     // return;
+
+    let finalAmount = 0
+
+    if( couponApplied && totalAfterDiscount ) {
+        finalAmount = Math.round(totalAfterDiscount * 100 );
+    } else {
+        finalAmount = Math.round(cartTotal * 100);
+    }
+
     //create payment intent with order amount and currency
 
     const paymentIntent = await stripe.paymentIntents.create({
-        amount: cartTotal * 100,
+        amount: finalAmount,
         currency: "usd",
     });
     res.send({
         clientSecret: paymentIntent.client_secret,
+        cartTotal,
+        totalAfterDiscount,
+        payable: finalAmount,
     });
 };
