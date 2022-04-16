d664938f8c4f82a762b698d608eaba380506e92c
duongdo238 <duongdo238@gmail.com>
2022-04-16 09:16:53 +0700

    load all orders

--- server/controllers/admin.js
+++ server/controllers/admin.js
@@ -1,19 +1,20 @@
+const Order = require('../models/order')
 const User = require('../models/user');
 
 exports.orders = async (req, res) => {
-    let orders = await orders
+    let allOrders = await Order
     .find({})
     .sort('-createdAt')
     .populate('products.product')
     .exec()
 
-    res.json(orders);
+    res.json(allOrders);
 };
 
 exports.orderStatus= async (req, res) => {
     const {orderId, orderStatus} = req.body;
 
-    let updated = await order
+    let updated = await Order
     .findByIdAndUpdate(orderId, {orderStatus}, {new: true})
     .exec()
     
