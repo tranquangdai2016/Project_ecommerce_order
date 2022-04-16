466be2c4713498df610264e936ab6f8190739fcb
duongdo238 <duongdo238@gmail.com>
2022-04-16 11:10:33 +0700

    wishlist

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,10 +2,20 @@ const express = require('express')
 
 const router = express.Router()
 
-router.get("/user", (req, res) => {
-    res.json({
-        data: "hey you hit user API endpoint",
-    });
-});
+//middlewares
+const {authCheck} = require ('../middlewares/auth')
+
+//controllers
+
+//wishlist 
+router.post('/user/wishlist',authCheck, addToWishlist);
+router.get('/user/wishlist',authCheck, wishlist);
+router.put('/user/wishlist/:productId',authCheck, removeFromWishlist);
+
+// router.get("/user", (req, res) => {
+//     res.json({
+//         data: "hey you hit user API endpoint",
+//     });
+// });
 
 module.exports = router;
