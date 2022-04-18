182f97129f021e6ef784f2a55b2694deba7a355a
long <longnv@pmr.vn>
2022-04-18 09:37:54 +0700

    [update]order

--- client/src/functions/stripe.js
+++ client/src/functions/stripe.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const createPaymentIntent = ( authtoken, coupon ) =>
     axios.post(
-        `${process.env.REACT_APP_API}/create-payment-intent`,
+        `${ process.env.REACT_APP_API }/create-payment-intent`,
         {couponApplied: coupon},
         {
             headers: {
