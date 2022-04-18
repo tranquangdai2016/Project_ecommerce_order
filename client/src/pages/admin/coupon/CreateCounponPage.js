7f95b5251bd19d8a9e0e9626e40c69e0896a965b
tranquangdai2016 <72093847+tranquangdai2016@users.noreply.github.com>
2022-04-18 16:19:17 +0700

    coupon list and delete

--- client/src/pages/admin/coupon/CreateCounponPage.js
+++ client/src/pages/admin/coupon/CreateCounponPage.js
@@ -3,7 +3,7 @@ import {useSelector, useDispatch} from "react-redux"
 import {toast} from "react-toastify"
 import DatePicker from 'react-datepicker'
 import {
-    getCounpons, 
+    getCoupons, 
     removeCoupon, 
     createCoupon
 } from '../../../functions/coupon'
@@ -16,10 +16,16 @@ const CreateCounponPage = () => {
     const [expiry,setExpiry] = useState("");
     const [discount,setDiscount] = useState("");
     const [loading,setLoading] = useState("");
+    const [coupons,setCoupons] = useState([]);
 
     //redux
 
     const { user } = useSelector((state) => ({...state }));
+
+    useEffect(() => {
+        loadAllCoupons();
+    },[]);
+    const loadAllCoupons = () => getCoupons().then((res) => setCoupons(res.data));
     const handleSubmit = (e) => {
         e.preventDefault();
         setLoading(true);
@@ -28,12 +34,26 @@ const CreateCounponPage = () => {
             {name, expiry, discount}, user.token)
         .then((res) => {
             setLoading(false);
+            loadAllCoupons(); //load all coupons
             setName("");
             setDiscount("");
             setExpiry("");
-            toString.success(`"${res.data.name}" is created`);
+            toast.success(`"${res.data.name}" is created`);
         })
         .catch((err) => console.log("create coupon err", err))
+    };
+
+    const handleRemove = couponId => {
+        if(window.confirm("Delete?")) {
+            setLoading(true);
+            removeCoupon(couponId, user.token)
+            .then((res) => {
+                loadAllCoupons(); //load all coupons
+                setLoading(false);
+                toast.error(`Coupon "${res.data.name}" deleted`);
+            })
+            .catch((err) => console.log(err));
+        }
     };
     return (
         <div className="container-fluid">
@@ -42,8 +62,11 @@ const CreateCounponPage = () => {
                     <AdminNav />
                 </div>
 
-                <div className="col-md-10">
-                    <h4>Coupon</h4>
+                <div className="col-md-10">\
+                {loading ? (
+                    <h4 className="text-danger">loading ...</h4>
+                ) : (<h4>Coupon</h4>) }
+                    
                     <form onSubmit={handleSubmit}>
                         <div className="form-group">
                             <label for="" className="text-muted">Name</label>
@@ -83,10 +106,38 @@ const CreateCounponPage = () => {
                             onChange={(date) => setExpiry(date)}
                             required
                             />
-
-                            <button className="btn btn-outline-primary">save</button>
                         </div>
+                        <button className="btn btn-outline-primary">save</button>
                     </form>
+
+                    <br />
+                    <h4>{coupons.length} Coupons</h4>
+
+                    <table className="table table-bodered"> 
+                        <thead>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Expire</th>
+                                <th scope="col">Discount</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {coupons.map((c) => {
+                                <tr key={c._id}>
+                                    <td>{c.name}</td>
+                                    <td>{new Date(c.expiry).toDateString()}</td>
+                                    <td>{c.discount}</td>
+                                    <td>
+                                        <DeleteOutlined 
+                                        onClick={() => handleRemove(c._id)} 
+                                        className="text-danger pointer" 
+                                        />
+                                    </td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
