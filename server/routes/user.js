195729ff79fa3e0466480f9d3c6f129d1b86d667
minhpv <thaitvph10585@fpt.edu.vn>
2022-04-24 22:28:06 +0700

    fix route

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,17 +11,11 @@ const {
   saveAddress,
   applyCouponToUserCart,
   createOrder,
-<<<<<<< HEAD
-  createCashOrder
-=======
+  createCashOrder,
   orders,
-<<<<<<< HEAD
->>>>>>> 89d428e6e2c8c5c8e468b8b8cb0d00d3e3ca73d2
-=======
   addToWishlist,
   wishlist,
   removeFromWishlist
->>>>>>> a790d222433b5be15894f79ad1989593bfd4cc9b
 } = require("../controllers/user");
 
 router.post("/user/cart", authCheck, userCart); //save cart
@@ -29,13 +23,10 @@ router.get("/user/cart", authCheck, getUserCart); //get cart
 router.delete("/user/cart", authCheck, emptyCart); //empty cart
 router.post("/user/address", authCheck, saveAddress);
 
-<<<<<<< HEAD
 router.post('user/order', authCheck, createOrder); //stripe
 router.post('user/cash-order', authCheck, createCashOrder); //cod
-=======
 router.post('user/order', authCheck, createOrder);
 router.get('user/orders', authCheck, orders);
->>>>>>> 89d428e6e2c8c5c8e468b8b8cb0d00d3e3ca73d2
 
 //coupon
 
