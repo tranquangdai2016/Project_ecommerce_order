c7d730767a3aa6901d33c59fbaf36d051cf0101d
danhnd98 <danh.nd1998@gmail.com>
2022-05-11 00:06:36 +0700

    [fix] authen

--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import setAuthToken from '../utils/setAuthToken'
 export const createOrUpdateUser = async (authtoken) => {
   return await axios.post(
     `${process.env.REACT_APP_API}/create-or-update-user`,
